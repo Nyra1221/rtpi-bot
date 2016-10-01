3445a4cd852cc33f0aa27ab5fb07372596b161ba
Ryan <ryan.werner2@mail.dcu.ie>
2016-10-01 19:46:27 +0100

    AHHH

--- fetch.py
+++ fetch.py
@@ -12,7 +12,9 @@ def fetchtime(stopnum):
         return "Sorry, there's no real time info for this stop!"
 
     if len(info["results"]) > 5:
-        split_into_pairs_of_5(info) 
+        while i < 5:
+            n.append("Route:" + " " + str(info['results'][i]['route']) + " " + "to" + " " + str(info['results'][i]['destination']) + "\n" + "Due:" + " " + str(info["results"][i]["duetime"]) + " " + "minutes." + "\n")
+            i = i + 1 
 
     else:
         while i < len(info["results"]):
@@ -21,25 +23,3 @@ def fetchtime(stopnum):
             i = i + 1 
     return '\n'.join(str(x) for x in n)
 
-
-def split_into_pairs_of_5(info)
-    outer = []
-    inner = []
-    for x in range(len(info["results"])):
-        if x % 5 == 0 and inner:
-            outer.append(inner)
-            inner = []
-
-        inner.append("Route:" + " " + str(info['results'][i]['route']) + " " + "to" + " " + str(info['results'][i]['destination']) + "\n" + "Due:" + " " + str(info["results"][i]["duetime"]) + " " + "minutes." + "\n")
-
-    if inner:
-        outer.append(inner)
-        inner = []
-
-    return outer
-
-
-result = split_into_pairs_of_5(info)
-
-for i in info:
-    send_message('\n'.join(str(x) for x in i))
