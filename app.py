64da77403e82a03cc8290c9ae81067b442f7b29a
Ryan <ryan.werner2@mail.dcu.ie>
2016-10-02 21:28:30 +0100

    ignores images

--- app.py
+++ app.py
@@ -38,9 +38,13 @@ def webhook():
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    tobesent = messaging_event["message"]["text"]
+                    recipient_id = messaging_event["recipient"]["id"] 
+                    try:
+                     # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        tobesent = messaging_event["message"]["text"]
+                    except:
+                        pass
 
                     send_message(sender_id, fetch.fetchtime(tobesent))
 
