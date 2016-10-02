8e5b615eb30ff1c49321ea7c694832ae75eeb113
Ryan <ryan.werner2@mail.dcu.ie>
2016-10-02 22:33:43 +0100

    fuckkk

--- app.py
+++ app.py
@@ -36,8 +36,10 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-                    if messaging_event["message"]["type"] == "image":
-                        send_message(sender_id, "I don't know what that is!")
+                    for thing in entry:
+                        if thing == "image":
+
+                            send_message(sender_id, "I don't know what that is!")
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
