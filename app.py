378dca6d5ba3b56d6101348f2e0d6eb01135adb2
Ryan <ryan.werner2@mail.dcu.ie>
2016-10-02 23:06:17 +0100

    fuck

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):
-                    if messaging_event["attachments"]["type"] == "image":
+                    if messaging_event["message"][3]["type"] == "image":
                         send_message(sender_id, "no")
                     else:  # someone sent us a message
 
